a0d1be85b32abc4c9a337e31b8824a0571c5bd68
tinhh github <tinhhdev@gmail.com>
2025-03-01 23:50:29 +0700

    ++ solve 2460

---
+++ src/main/java/vn/tinhh/leetcodesolve/Lc2451To2500.java
@@ -0,0 +1,35 @@
+package vn.tinhh.leetcodesolve;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Lc2451To2500 {
+
+    // 2460
+    public int[] applyOperations(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] = nums[i] * 2;
+                nums[i + 1] = 0;
+            }
+        }
+        int lastPos = nums.length - 1;
+        for (int i = 0; i <= lastPos; i++) {
+            if (nums[i] == 0) {
+                for (int j = i; j <= lastPos; j++) {
+                    if (j == lastPos) {
+                        nums[j] = 0;
+                    } else {
+                        nums[j] = nums[j + 1];
+                    }
+                }
+                i=i-1;
+                lastPos = lastPos - 1;
+            }
+        }
+        return nums;
+    }
+}
---
+++ src/test/java/vn/tinhh/leetcodesolve/Lc2451To2500Test.java
@@ -0,0 +1,45 @@
+package vn.tinhh.leetcodesolve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class Lc2451To2500Test {
+
+    @Autowired
+    private Lc2451To2500 Lc2451To2500;
+
+    @Test
+    void LC2460_case1() {
+        int[] nums = {1,2,2,1,1,0};
+        int[] rtValue= Lc2451To2500.applyOperations(nums);
+
+        int[] expected = {1,4,2,0,0,0};
+        assertTrue(Arrays.equals(rtValue,expected));
+    }
+
+    @Test
+    void LC2460_case2() {
+        int[] nums = {0,1};
+        int[] rtValue= Lc2451To2500.applyOperations(nums);
+
+        int[] expected = {1,0};
+        assertTrue(Arrays.equals(rtValue,expected));
+    }
+
+
+    @Test
+    void LC2460_case3() {
+        int[] nums = {847,847,0,0,0,399,416,416,879,879,206,206,206,272};
+        int[] rtValue= Lc2451To2500.applyOperations(nums);
+
+        int[] expected = {1694,399,832,1758,412,206,272,0,0,0,0,0,0,0};
+        assertTrue(Arrays.equals(rtValue,expected));
+    }
+}
+
