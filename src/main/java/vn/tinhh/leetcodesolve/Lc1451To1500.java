13297017aa05cb175da5228357b6c2951b4e70aa
tinhh <tinhhdev@gmail.com>
2025-04-29 14:49:08 +0700

    ++ 1455

--- src/main/java/vn/tinhh/leetcodesolve/Lc1451To1500.java
+++ src/main/java/vn/tinhh/leetcodesolve/Lc1451To1500.java
@@ -8,6 +8,15 @@ public class Lc1451To1500 {
     //1455
     //https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/?envType=daily-question&envId=2025-04-29
     public int isPrefixOfWord(String sentence, String searchWord) {
-
+        if (!sentence.contains(searchWord)) {
+            return -1;
+        }
+        String[] springSplit = sentence.split(" ");
+        for (int i = 0; i < springSplit.length; i++) {
+            if (springSplit[i].startsWith(searchWord)) {
+                return i+1;
+            }
+        }
+        return -1;
     }
 }
--- src/test/java/vn/tinhh/leetcodesolve/Lc1451To1500Test.java
+++ src/test/java/vn/tinhh/leetcodesolve/Lc1451To1500Test.java
@@ -1,5 +1,6 @@
 package vn.tinhh.leetcodesolve;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,5 +12,30 @@ class Lc1451To1500Test {
     @Autowired
     private Lc1451To1500 lc1451To1500;
 
+    @Test
+    void LC1455_case1() {
+        String sentence = "i love eating burger";
+        String searchWord = "burg";
+        int rtValue = lc1451To1500.isPrefixOfWord(sentence, searchWord);
+        int expect = 4;
+        assertTrue(rtValue == expect);
+    }
 
+    @Test
+    void LC1455_case2() {
+        String sentence = "this problem is an easy problem";
+        String searchWord = "pro";
+        int rtValue = lc1451To1500.isPrefixOfWord(sentence, searchWord);
+        int expect = 2;
+        assertTrue(rtValue == expect);
+    }
+
+    @Test
+    void LC1455_case3() {
+        String sentence = "i am tired";
+        String searchWord = "you";
+        int rtValue = lc1451To1500.isPrefixOfWord(sentence, searchWord);
+        int expect = -1;
+        assertTrue(rtValue == expect);
+    }
 }
