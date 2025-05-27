1b5030b1e6566dddb8eb7fe918601c30f0ed5ed6
tinhh <tinhhdev@gmail.com>
2025-05-27 09:26:07 +0700

    ++ 2894

--- src/main/java/vn/tinhh/leetcodesolve/Lc2851To2900.java
+++ src/main/java/vn/tinhh/leetcodesolve/Lc2851To2900.java
@@ -8,6 +8,20 @@ import java.util.List;
 @Component
 public class Lc2851To2900 {
 
+    //2894
+    //https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/?envType=daily-question&envId=2025-05-27
+    public int differenceOfSums(int n, int m) {
+        int rtValue = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) {
+                rtValue-=i;
+            } else {
+                rtValue+=i;
+            }
+        }
+        return rtValue;
+    }
+
     //2900
     //https://leetcode.com/problems/longest-unequal-adjacent-groups-subsequence-i/description/?envType=daily-question&envId=2025-05-15
     public List<String> getLongestSubsequence(String[] words, int[] groups) {
--- src/test/java/vn/tinhh/leetcodesolve/Lc2851To2900Test.java
+++ src/test/java/vn/tinhh/leetcodesolve/Lc2851To2900Test.java
@@ -15,20 +15,47 @@ class Lc2851To2900Test {
     private Lc2851To2900 lc2851To2900;
 
     @Test
+    void LC2894_case1() {
+        int n = 10;
+        int m = 3;
+        int rtValue = lc2851To2900.differenceOfSums(n, m);
+        int expected = 19;
+        assertEquals(rtValue, expected);
+    }
+
+    @Test
+    void LC2894_case2() {
+        int n = 5;
+        int m = 6;
+        int rtValue = lc2851To2900.differenceOfSums(n, m);
+        int expected = 15;
+        assertEquals(rtValue, expected);
+    }
+
+    @Test
+    void LC2894_case3() {
+        int n = 5;
+        int m = 1;
+        int rtValue = lc2851To2900.differenceOfSums(n, m);
+        int expected = -15;
+        assertEquals(rtValue, expected);
+    }
+
+    @Test
     void LC2900_case1() {
-        String[] words = {"e","a","b"};
-        int[] groups = {0,0,1};
+        String[] words = {"e", "a", "b"};
+        int[] groups = {0, 0, 1};
         List<String> rtValue = lc2851To2900.getLongestSubsequence(words, groups);
-        List<String> expected =List.of("e","b");
+        List<String> expected = List.of("e", "b");
         assertEquals(rtValue, expected);
     }
 
     @Test
     void LC2900_case2() {
-        String[] words = {"a","b","c","d"};
-        int[] groups = {1,0,1,1};
+        String[] words = {"a", "b", "c", "d"};
+        int[] groups = {1, 0, 1, 1};
         List<String> rtValue = lc2851To2900.getLongestSubsequence(words, groups);
-        List<String> expected =List.of("a","b","c");
+        List<String> expected = List.of("a", "b", "c");
         assertEquals(rtValue, expected);
     }
 }
